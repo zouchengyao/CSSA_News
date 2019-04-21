09a6e72503f4ca161e2028b6a70146eafa7510f8
cz5 <cz5@illinois.edu>
2019-04-20 21:36:58 -0500

    update email send entry

--- YouNews/Settings.swift
+++ YouNews/Settings.swift
@@ -10,7 +10,7 @@
 
 import UIKit
 import Parse
-
+import MessageUI
 
 // MARK: - SETTINGS CELL
 class SettingsCell: UITableViewCell {
@@ -26,167 +26,202 @@ class SettingsCell: UITableViewCell {
 
 // MARK: - SETTINGS CONTROLLER
 class Settings: UIViewController,
-UITableViewDelegate,
-UITableViewDataSource
+    UITableViewDelegate,
+    UITableViewDataSource,
+    MFMailComposeViewControllerDelegate
 {
-
+    
     /* Views */
     @IBOutlet weak var settingsTableView: UITableView!
     
     
-
- 
-override func viewDidAppear(_ animated: Bool) {
-    settingsTableView.reloadData()
-}
-
-    
-override func viewDidLoad() {
-        super.viewDidLoad()
-
-}
-
     
     
-// MARK: - TABLEVIEW DELEGATES
-    //Set the number of section
-func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-}
-    //Set the number of cells to be displayed in a section
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 6
-}
-  //Initialize cells
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    // Downcast each cell as a SettingsCell, which has an upper label, a lower label, and an image.
-    let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsCell
-    
-    // Round cell image
-    cell.sImage.layer.cornerRadius = cell.sImage.bounds.size.width/2
-    
-    
-    // Set the labels and images to be displayed in each cell
-    switch indexPath.row {
-        
-    case 0:
-        cell.upperLabel.text = "Your Profile"
-        if PFUser.current() != nil {
-            // Display user's username and get user's avatar if the user has logged in
-            cell.lowerLabel.text = PFUser.current()!.username
-            let imageFile = PFUser.current()![USER_AVATAR] as? PFFile
-            imageFile?.getDataInBackground(block: { (data, error) in
-                if error == nil { if let imageData = data {
-                    // Display user's avatar
-                    cell.sImage.image = UIImage(data: imageData)
-            }}})
-            
-        } else {
-            // Display logo and login instructions if the user hasn't logged in
-            cell.sImage.image = UIImage(named: "logo")
-            cell.lowerLabel.text = "Tap to Login or Sign Up"
-        }
-    
-
-    case 1:
-        cell.sImage.image = UIImage(named: "application_icon")
-        cell.upperLabel.text = "Application Name"
-        cell.lowerLabel.text = APP_NAME
-
-    case 2:
-        cell.sImage.image = UIImage(named: "info_icon")
-        cell.upperLabel.text = "Build Version"
-        cell.lowerLabel.text = "Version 1.0"
-        
-    case 3:
-        cell.sImage.image = UIImage(named: "mail_icon")
-        cell.upperLabel.text = "Contact Email"
-        cell.lowerLabel.text = YOUR_CONTACT_EMAIL
-        
-    case 4:
-        cell.sImage.image = UIImage(named: "rate_icon")
-        cell.upperLabel.text = "Rate App"
-        cell.lowerLabel.text = "Give us some feedback"
-        
-    case 5:
-        cell.sImage.image = UIImage(named: "share_icon")
-        cell.upperLabel.text = "Share \(APP_NAME)"
-        cell.lowerLabel.text = "Spread the word for this awesome app!"
-
-        
-    default:break
+    override func viewDidAppear(_ animated: Bool) {
+        settingsTableView.reloadData()
     }
     
     
-return cell
-}
-// Set height for each cell
-func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 80
-}
-    
-    
-// MARK: - CELL TAPPED
-func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    switch indexPath.row {
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-    // Go to your Profile
-    case 0:
+    }
+    
+    
+    
+    // MARK: - TABLEVIEW DELEGATES
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6 // This is the number of cells that will be displayed
+        // 需要加cell的 这里需要修改
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath) as! SettingsCell
+        
+        // Round cell image
+        cell.sImage.layer.cornerRadius = cell.sImage.bounds.size.width/2
+        
+        
+        // Init cells
+        switch indexPath.row {
+            
+        case 0:
+            cell.upperLabel.text = "Your Profile"
             if PFUser.current() != nil {
-                // Get into Profile interface
+                cell.lowerLabel.text = PFUser.current()!.username
+                let imageFile = PFUser.current()![USER_AVATAR] as? PFFile
+                imageFile?.getDataInBackground(block: { (data, error) in
+                    if error == nil { if let imageData = data {
+                        cell.sImage.image = UIImage(data: imageData)
+                        }}})
+                
+            } else {
+                cell.sImage.image = UIImage(named: "logo")
+                cell.lowerLabel.text = "Tap to Login or Sign Up"
+            }
+            
+            
+        case 1:
+            cell.sImage.image = UIImage(named: "application_icon")
+            cell.upperLabel.text = "TEST 1 留空"
+            cell.lowerLabel.text = APP_NAME
+            
+        case 2:
+            cell.sImage.image = UIImage(named: "info_icon")
+            cell.upperLabel.text = "TEST 2 留空"
+            cell.lowerLabel.text = "Version 1.0"
+            
+        case 3:
+            cell.sImage.image = UIImage(named: "mail_icon")
+            cell.upperLabel.text = "联系CSSA"
+            cell.lowerLabel.text = YOUR_CONTACT_EMAIL
+            
+        case 4:
+            cell.sImage.image = UIImage(named: "rate_icon")
+            cell.upperLabel.text = "跳转到CSSA网页"
+            cell.lowerLabel.text = "看看有什么新内容！"
+            
+        case 5:
+            cell.sImage.image = UIImage(named: "share_icon")
+            cell.upperLabel.text = "Share \(APP_NAME)"
+            cell.lowerLabel.text = "分享CSSA给大家吧！"
+            
+            
+        default:break
+        }
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    
+    // MARK: - CELL TAPPED
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+            
+        // Go to your Profile
+        case 0:
+            if PFUser.current() != nil {
                 let aVC = storyboard?.instantiateViewController(withIdentifier: "Profile") as! Profile
                 navigationController?.pushViewController(aVC, animated: true)
             } else {
-                // Get into Login interface
                 let aVC = storyboard?.instantiateViewController(withIdentifier: "Login") as! Login
                 present(aVC, animated: true, completion: nil)
             }
-        
-        
-    // Copy your contact email address to clipboard
-    case 3:
+            
+            
+        //
+        case 1:
+            print("reached here")
+            if !MFMailComposeViewController.canSendMail() {
+                print("Mail services are not available")
+                return
+            }
+            let composeVC = MFMailComposeViewController()
+            composeVC.mailComposeDelegate = self
+            
+            // Configure the fields of the interface.
+            composeVC.setToRecipients(["address@example.com"])
+            composeVC.setSubject("Hello!")
+            composeVC.setMessageBody("Hello from California!", isHTML: false)
+            
+            // Present the view controller modally.
+            self.present(composeVC, animated: true, completion: nil)
+            
+            print("asfasdfasf")
+            
+            
+            
+            
+            
+            
+        // Copy your contact email address to clipboard
+        case 3:
             UIPasteboard.general.string = YOUR_CONTACT_EMAIL
-            simpleAlert("Our email address has been copied to your clipboard. Open your favorite Mail app and send us a message, we'd love to hear from you!")
-        
-        
-    // Go to the App Store Ratings page of this app
-    case 4:
-            let aURL = URL(string: "itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=" + YOUR_APP_ID)
+            simpleAlert("CSSA官方邮箱地址已复制到粘贴板。\n与CSSA分享你的意见和建议吧！😊")
+            
+            
+            // Old: Go to the App Store Ratings page of this app
+        // New: GOTO CSSA webpage: http://uiuccssa.org/2018/
+        case 4:
+            let aURL = URL(string: "http://uiuccssa.org/2018/")
             UIApplication.shared.openURL(aURL!)
-
-        
-    // Share this app
-    case 5:
-        let messageStr  = "Get #" + APP_NAME + " on the App Store " + YOUR_APP_STORE_LINK
-        let img = UIImage(named: "logo")!
-        // Combine messageStr and img as shareItems
-        let shareItems = [messageStr, img] as [Any]
-        
-        let activityViewController = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
-        // Exclude the following activitytypes
-        activityViewController.excludedActivityTypes = [.print, .postToWeibo, .copyToPasteboard, .addToReadingList, .postToVimeo]
-        // Share with others
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            // iPad
-            let popOver = UIPopoverController(contentViewController: activityViewController)
-            popOver.present(from: .zero, in: self.view, permittedArrowDirections: .any, animated: true)
-        } else {
-            // iPhone
-            present(activityViewController, animated: true, completion: nil)
+            
+            
+        // Share this app
+        case 5:
+            let messageStr  = "Get #" + APP_NAME + " on the App Store " + YOUR_APP_STORE_LINK
+            let img = UIImage(named: "logo")!
+            
+            let shareItems = [messageStr, img] as [Any]
+            
+            let activityViewController = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+            activityViewController.excludedActivityTypes = [.print, .postToWeibo, .copyToPasteboard, .addToReadingList, .postToVimeo]
+            
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                // iPad
+                let popOver = UIPopoverController(contentViewController: activityViewController)
+                popOver.present(from: .zero, in: self.view, permittedArrowDirections: .any, animated: true)
+            } else {
+                // iPhone
+                present(activityViewController, animated: true, completion: nil)
+            }
+            
+            
+        default:break
         }
-
         
-    default:break
     }
- 
-}
-    
-
     
     
     
-override func didReceiveMemoryWarning() {
+    
+    
+    override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
+
+
+
+func mailComposeController(controller: MFMailComposeViewController,
+                           didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+    // Check the result or perform other tasks.
+    
+    
+    
+    
+    // Dismiss the mail compose view controller.
+    controller.dismiss(animated: true, completion: nil)
+}
+
